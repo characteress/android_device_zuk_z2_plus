b8a8e477da57fe33eed2bcf1f56f38e4163dea75
Mourya Baruah <mouryabaruah@gmail.com>
2017-08-10 21:17:23 +0530

    Update aosp_z2_plus.mk

--- aosp_z2_plus.mk
+++ aosp_z2_plus.mk
@@ -16,10 +16,17 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product-if-exists, vendor/zuk/z2_plus/device-vendor.mk)
 
 # Inherit from z2_plus device
 $(call inherit-product, device/zuk/z2_plus/device.mk)
+
+$(call inherit-product-if-exists, vendor/zuk/z2_plus/z2_plus-vendor.mk)
+
+# Include Google Apps package
+include vendor/gapps/gapps.mk
+include vendor/gapps/prebuilt.mk
+include vendor/gapps/common-blobs.mk
+
 
 PRODUCT_NAME := aosp_z2_plus
 PRODUCT_DEVICE := z2_plus
