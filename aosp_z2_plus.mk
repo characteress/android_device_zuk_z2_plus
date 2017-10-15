06194fad7b1832cb7745e316302390d5ca788c7e
devabhi <devabhixda@gmail.com>
2017-10-15 07:15:29 +0530

    fusion bringup

--- aosp_z2_plus.mk
+++ aosp_z2_plus.mk
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Include pure telephony configuration
+$(call inherit-product, vendor/pure/configs/pure_phone.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
